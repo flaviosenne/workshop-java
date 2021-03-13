af509f0844a3623bbe3547196e0e47710a5c1dec
flaviosenne123 <joaoflausino123@hotmail.com>
2021-03-12 22:31:52 -0300

    return responseEntity

--- src/main/java/com/workshop/api/resources/UserController.java
+++ src/main/java/com/workshop/api/resources/UserController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import com.workshop.api.domain.User;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     
     @GetMapping
-    public List<User> findAll(){
+    public ResponseEntity<List<User>> findAll(){
         User maria = new User(1, "Maria Brown", "maria@email.com");
         User alex = new User(2, "Alex Green", "alex@email.com");
          List<User> list = new ArrayList<>();
          list.addAll(Arrays.asList(maria, alex));
 
-         return list;
+         return ResponseEntity.status(200).body(list);
     }
 }
